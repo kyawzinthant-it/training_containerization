4b36852b8f1b0edfac82ec8973cafc01ef13574a
Chandra Shekhar Goka <chandrashekhar.goka@gmail.com>
2019-02-03 12:53:36 +0530

    Initial commit

---
+++ src/main/java/com/onlinetutorialspoint/Springbootdocker/HelloController.java
@@ -0,0 +1,12 @@
+package com.onlinetutorialspoint.Springbootdocker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello from docker";
+    }
+}
